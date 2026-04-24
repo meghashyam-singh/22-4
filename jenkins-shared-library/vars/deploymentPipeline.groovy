9ecf8d968c7d7705588a4751365086968d5539cc
megha <megha@gmail.com>
2026-04-24 16:56:19 +0530

    22-4

---
+++ jenkins-shared-library/vars/deploymentPipeline.groovy
@@ -0,0 +1,44 @@
+def call(configMap) {
+    pipeline {
+        agent {
+            node {
+                label 'AGENT-1'
+            }
+        }
+        environment {
+            REGION = "us-east-1"
+            COMPONENT = "${configMap.COMPONENT}"
+            CLUSTER_NAME = "roboshop-cluster"
+            BRANCH = "${configMap.BRANCH}"
+            GIT_URL = "${configMap.GIT_URL}"
+        }
+        stages {
+            stage('get code') {
+                steps {
+                    git url: "${GIT_URL}", branch: "${BRANCH}"
+                }
+            }
+            stage('deployment') {
+                steps {
+                    dir("${COMPONENT}") {
+                        script {
+                            withAWS(region:"${REGION}",credentials:'aws-creds') {
+                                sh """
+                                aws eks update-kubeconfig --region ${REGION} --name ${CLUSTER_NAME}
+                                kubectl apply -f manifestfile.yaml
+                                """
+                            }
+                        }
+                    }
+                }
+            }
+            stage('health check') {
+                steps {
+                    withAWS(region:"${REGION}",credentials:'aws-creds') {
+                        sh " kubectl rollout status deployment ${COMPONENT}"
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ jenkins-shared-library/vars/pythonPipeline.groovy
@@ -0,0 +1,105 @@
+def call(Map configMap) {
+    pipeline {
+        agent {
+            node {
+                label 'AGENT-1'
+            }
+        }
+        environment {
+            APPVERSION = ""
+            REGION = "us-east-1"
+            ACCOUNT_ID = "515138251473"
+            COMPONENT = "${configMap.COMPONENT}"
+            PROJECT = "roboshop"
+            BRANCH = "${configMap.BRANCH}"
+            GIT_URL = "${configMap.GIT_URL}"
+        }
+        options {
+            timeout(time:15, unit: 'MINUTES')
+            disableConcurrentBuilds()
+        }
+        stages {
+            stage('clean workspace') {
+                steps {
+                    cleanWs()
+                }
+            }
+            stage('get code') {
+                steps {
+                    git url: "${GIT_URL}", branch: "${BRANCH}"
+                }
+            }
+            stage('read version') {
+                steps {
+                    dir("${COMPONENT}") {
+                        script {
+                            APPVERSION = readFile ('version.txt').trim()
+                            echo "APPVERSION IS: ${appversion}"
+                        }
+                    }
+                }
+            }
+            stage('build code') {
+                steps {
+                    dir("${COMPONENT}") {
+                        sh "pip3 install -r requirements.txt"
+                    }
+                }
+            }
+            stage('sonarqube') {
+                steps {
+                    dir("${COMPONENT}") {
+                        script {
+                            def scannerHome = tool 'sonar-8.0'
+                            withSonarQubeEnv('sonar-server') {
+                                sh "${scannerHome}/bin/sonar-scanner"
+                            }
+                        }
+                    }
+                }
+            }
+            stage('quality stage') {
+                steps {
+                    script {
+                        timeout(time:15, unit: 'MINUTES') {
+                            waitForQualityGate abortPipeline: true
+                        }
+                    }
+                }
+            }
+            stage('image-build') {
+                steps {
+                    sh """
+                    docker build -t ${PROJECT}/${COMPONENT}:${APPVERSION}-${BUILD_NUMBER} ./${COMPONENT}
+                    docker images
+                    """
+                }
+            }
+            stage('image-scan') {
+                steps {
+                    sh "trivy image ${PROJECT}/${COMPONENT}:${APPVERSION}-${BUILD_NUMBER} > ${COMPONENT}-image-scan-report-txt"
+                }
+            }
+            stage('image push') {
+                steps {
+                    script {
+                        withAWS(region:"${REGION}",credentials:'aws-creds') {
+                            sh """
+                            aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
+                            docker tag ${PROJECT}/${COMPONENT}:${APPVERSION}-${BUILD_NUMBER} ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${PROJECT}/${COMPONENT}:${APPVERSION}-${BUILD_NUMBER}
+                            docker push ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${PROJECT}/${COMPONENT}:${APPVERSION}-${BUILD_NUMBER}
+                            """
+                        }
+                    }
+                }
+            }
+            stage('trigger deployment') {
+                steps {
+                    build job: "${COMPONENT}-cd-pipeline",
+                    propagate: false,
+                    wait: false
+                }
+            }
+        }
+    }
+}
